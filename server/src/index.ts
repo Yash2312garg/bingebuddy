8eecd2d93b40721adce9537ed2f862c6b4ad8bcb
Yash Garg <ygarg@konrad.com>
2026-06-15 18:59:56 +0530

    made some changes to check the github action

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,70 +1,61 @@
-import express from 'express';
-import type { Response, Request } from 'express';
-import { pool } from './database/db';
-import { redisClient } from './database/redis';
-import { redisStreamClient } from './modules/notifications/redis.config';
-import { RestaurantLoginSession } from './sessions/RestaurantLoginSession';
-import { notificationRouter } from './modules/notifications';
-import restaurantPreLoginRoutes from "./routes/restaurant/prelogin.routes"; 
+import express from "express";
+import type { Response, Request } from "express";
+import { pool } from "./database/db";
+import { redisClient } from "./database/redis";
+import { redisStreamClient } from "./modules/notifications/redis.config";
+import { RestaurantLoginSession } from "./sessions/RestaurantLoginSession";
+import { notificationRouter } from "./modules/notifications";
+import restaurantPreLoginRoutes from "./routes/restaurant/prelogin.routes";
 import restaurantMenuRoutes from "./routes/restaurant/menu.routes";
 import restaurantCategoryRoutes from "./routes/restaurant/categories.routes";
 import restaurantAuthRoutes from "./routes/auth/auth.routes";
-import restaurantInfoRoutes from "./routes/restaurant/restaurant.routes"
-import cookieParser from 'cookie-parser';
-import 'dotenv/config'; 
-import "./types/express-session"
-import helmet from 'helmet';
-import cors from "cors"
+import restaurantInfoRoutes from "./routes/restaurant/restaurant.routes";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+import "./types/express-session";
+import helmet from "helmet";
+import cors from "cors";
 
 const app = express();
 
-
-app.use(RestaurantLoginSession)
-app.use(express.json())
-app.use(cookieParser())
-app.use(helmet())
-app.use( cors({
+app.use(RestaurantLoginSession);
+app.use(express.json());
+app.use(cookieParser());
+app.use(helmet());
+app.use(
+  cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-  })
-)
+  }),
+);
 
-app.use("/restaurant/prelogin",restaurantPreLoginRoutes);
-app.use("/restaurant/menu",restaurantMenuRoutes);
-app.use("/restaurant/categories",restaurantCategoryRoutes);
-app.use("/auth", restaurantAuthRoutes)
-app.use("/restaurant/info",restaurantInfoRoutes) 
+app.use("/restaurant/prelogin", restaurantPreLoginRoutes);
+app.use("/restaurant/menu", restaurantMenuRoutes);
+app.use("/restaurant/categories", restaurantCategoryRoutes);
+app.use("/auth", restaurantAuthRoutes);
+app.use("/restaurant/info", restaurantInfoRoutes);
 app.use("/api/v1/notifications", notificationRouter);
 
-app.get('/', (_req: Request, res:Response) => {
-    res.status(200).json('Hello World!');
+app.get("/", (_req: Request, res: Response) => {
+  res.status(200).json("Hello World!");
 });
 
-
-
-const startServer = async ()=>{
-    try{
-    await redisClient.connect()
-    console.log('Connected to Redis successfully.');
-    await redisStreamClient.connect()
-    console.log('connect to Redis Stream Cliend sucessfully')
+const startServer = async () => {
+  try {
+    await redisClient.connect();
+    // console.log("Connected to Redis successfully.");
+    await redisStreamClient.connect();
+    // console.log("connect to Redis Stream Cliend sucessfully");
     const Client = await pool.connect();
-    console.log('Connected to pg, succesfully')
-    Client.release()
+    console.log("Connected to pg, succesfully");
+    Client.release();
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("Server is running on port ", process.env.PORT || 3000);
+    });
+  } catch (e) {
+    // console.error("Failed to start the server:", e);
+    process.exit(1);
+  }
+};
 
-    // RedisConsumer.startWorkerLoop().catch((workerError) => {
-    //         console.error("🚨 CRITICAL: Notification Worker crashed unexpectedly:", workerError);
-    // })
-    app.listen(process.env.PORT||3000,()=>{
-    console.log('Server is running on port ',process.env.PORT||3000);
-})
-
-    }catch(e){
-        console.error('Failed to start the server:', e);
-        process.exit(1);
-    }
-
-}
-
-
-startServer()
+startServer();
